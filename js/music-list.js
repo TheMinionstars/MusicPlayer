3672bca5de194c1edfc52f1edab26fe59619d8b8
Đinh Quý Dương <duong@MacBook-Pro-cua-inh.local>
2023-01-22 09:26:48 +0700

    3rd change

--- js/music-list.js
+++ js/music-list.js
@@ -1,25 +1,20 @@
 let allMusic = [
   {
-    name: "Trời Giấu Trời Mang Đi",
-    artist: "Amee x ViruSs",
-    img: "music-1",
-    src: "music-1"
-  },
-  {
-    name: "Giấc Mơ Rất Thơ",
-    artist: "Mer",
-    img: "music-2",
-    src: "music-2"
-  },
-  {
-    name: "Tình Thật Tình Mơ",
-    artist: "Mer",
-    img: "music-3",
-    src: "music-3"
+    name: "9704 2292 0616 7315 130",
+    artist: "Đinh Quý Dương",
+    img: "1",
+    src: "1"
   },
 
   {
-    name: "Nhạc Tết",
+    name: "9704 2292 0616 7315 130",
+    artist: "Đinh Quý Dương",
+    img: "2",
+    src: "2"
+  },
+
+  {
+    name: "9704 2292 0616 7315 130",
     artist: "Đinh Quý Dương",
     img: "abc",
     src: "abc"
